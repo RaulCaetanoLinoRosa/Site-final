d0e48e676067e78480951975321054cf047ca537
RaulCaetanoLinoRosa <161162683+RaulCaetanoLinoRosa@users.noreply.github.com>
2024-03-07 15:07:38 -0300

    Nv

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('container');
+/*const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
@@ -8,4 +8,41 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
-});
+});*/
+
+(function(){
+    
+	const cep = document.querySelector("input[name=cep]");
+  
+  cep.addEventListener('blur', e=> {
+  		const value = cep.value.replace(/[^0-9]+/, '');
+      const url = `https://viacep.com.br/ws/${value}/json/`;
+      
+      fetch(url)
+      .then( response => response.json())
+      .then( json => {
+      		
+          if( json.logradouro ) {
+          	document.querySelector('input[name=rua]').value = json.logradouro;
+            document.querySelector('input[name=bairro]').value = json.bairro;
+            document.querySelector('input[name=cidade]').value = json.localidade;
+            document.querySelector('input[name=estado]').value = json.uf;
+          }     
+      });           
+  });
+})();
+
+function mascara(i){
+   
+  var v = i.value;
+  
+  if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+     i.value = v.substring(0, v.length-1);
+     return;
+  }
+  
+  i.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) i.value += ".";
+  if (v.length == 11) i.value += "-";
+
+}
